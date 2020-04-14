8a2e2dd26ac7c1a5eaa426ca411d357936c56534
ci11y <panic4thuy@gmail.com>
2020-04-13 22:44:08 -0700

    :sparkles: Add recursive readdir func

--- flatten.ml
+++ flatten.ml
@@ -15,10 +15,22 @@ let rec print_str_list l =
 let readdir_abs s_dir = 
     let file_names = Sys.readdir s_dir in
     let cwd = Sys.getcwd () in
-    Array.map (fun s -> cwd ^ s) file_names
+    let abs_files = Array.map (fun s -> cwd ^ "/" ^ s) file_names in
+    Array.to_list abs_files
+;;
+
+let rec rec_readdir_inner dir_list =
+    match dir_list with
+    [] -> []
+    | h::t -> 
+        if Sys.is_directory h then rec_readdir_inner t else h :: rec_readdir_inner t
+;;
+
+let rec_readdir s_dir = 
+    rec_readdir_inner (readdir_abs s_dir)
 ;;
 
 let () =
-    let dir_contents = readdir_abs "." in
-    print_str_list (Array.to_list dir_contents)
+    let dir_contents = rec_readdir "." in
+    print_str_list (dir_contents)
 ;;
