93b63c092d0fbddff5371c596b32147fc9f311b9
ci11y <panic4thuy@gmail.com>
2020-04-15 20:46:05 -0700

    :sparkles: Add a rename function

--- level.ml
+++ level.ml
@@ -4,7 +4,17 @@ open String
 open Sys
 
 open Pathlib
+open Stringlib
 
+let rec level cwd path =
+    match cwd with
+    "" -> Stringlib.replace path "/" "_"
+    |_ -> 
+        let len_cwd = String.length cwd in
+        let len_path = String.length path in
+        let cwd_0 = String.sub cwd 0 1 in
+        cwd_0 ^ level (String.sub cwd 1 (len_cwd - 1)) (String.sub path 1 (len_path - 1))
+;;
 
 let rec print_str_list l =
     match l with
@@ -29,5 +39,7 @@ let rec_readdir s_dir =
 let () =
     let dir_contents = rec_readdir "." in
     let abs_contents = Pathlib.comp2abs dir_contents in
-    print_str_list (abs_contents)
+    let cwd = Sys.getcwd() ^ "/" in
+    print_str_list (abs_contents);
+    print_str_list (List.map (level cwd) abs_contents)
 ;;
